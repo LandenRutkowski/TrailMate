a79b657d05977d61fab34c9ac4134150d8a780fb
LandenRutkowski <lbrutkowski34@gmail.com>
2023-12-05 21:46:28 -0500

    Moved Test Folder

--- app/src/androidTest/java/com.example.trailmate/ApplicationTest.java
+++ app/src/androidTest/java/com.example.trailmate/ApplicationTest.java
@@ -1,22 +1,17 @@
 package com.example.trailmate;
 
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
-import android.os.Looper;
-import android.support.test.rule.ActivityTestRule;
-import android.transition.Transition;
 
 import androidx.annotation.NonNull;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -25,13 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -43,81 +32,65 @@ import java.util.Map;
  */
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest {
-	private AddCoordinatesActivity activity = new AddCoordinatesActivity();
-	private HostActivity hostActivity = new HostActivity();
 
-	@Test
-	public void onCreate() {
-
-
-		assertNotNull(activity.mTitleEditText);
-		assertNotNull(activity.mLatitudeEditText);
-		assertNotNull(activity.mLongitudeEditText);
-		assertNotNull(activity.mDescriptionEditText);
-
-		assertNotNull(activity.mSaveButton);
-
-
-		assertNotNull(activity.mDatabase);
-
-
-		assertNotNull(activity.mRef);
-	}
-
-	@Test
-	public void onClickSaveButton() {
-
-		activity.mSaveButton.performClick();
-
-
-		assertEquals("", activity.mTitleEditText.getText().toString());
-		assertEquals("", activity.mLatitudeEditText.getText().toString());
-		assertEquals("", activity.mLongitudeEditText.getText().toString());
-		assertEquals("", activity.mDescriptionEditText.getText().toString());
-	}
-
-	@Test
-	public void onHostCreate() {
-
-		assertNotNull(hostActivity.logout);
-	}
-
+	private MainActivity mainActivity;
+	private HostActivity hostActivity;
 	private FirebaseAuth firebaseAuth;
 
 	@Before
 	public void setUp() {
-		// Initialize FirebaseAuth instance
-		firebaseAuth = FirebaseAuth.getInstance();
 
-		// Launch MainActivity before each test
-		ActivityScenario.launch(MainActivity.class);
-	}
 
-	@After
-	public void tearDown() {
-		// Sign out the user after each test
-		signOut();
 	}
+	@Test
+	public void onCreateTrail() {
+
+		CharSequence title = hostActivity.getTitle();
+		assertEquals(title, "Firebase.child.title");
+
+
+//		assertNotNull(activity.mTitleEditText);
+//		assertNotNull(activity.mLatitudeEditText);
+//		assertNotNull(activity.mLongitudeEditText);
+//		assertNotNull(activity.mDescriptionEditText);
+//		assertNotNull(activity.mSaveButton);
+//		assertNotNull(activity.mDatabase);
+//		assertNotNull(activity.mRef);
+	}
+//
+//	@Test
+//	public void onClickSaveButton() {
+//
+//		activity.mSaveButton.performClick();
+//
+//
+//		assertEquals("", activity.mTitleEditText.getText().toString());
+//		assertEquals("", activity.mLatitudeEditText.getText().toString());
+//		assertEquals("", activity.mLongitudeEditText.getText().toString());
+//		assertEquals("", activity.mDescriptionEditText.getText().toString());
+//	}
+//
+//	@Test
+//	public void onHostCreate() {
+//
+//		assertNotNull(hostActivity.logout);
+//	}
 
 	@Test
 	public void testLoginButton() {
-		// Assuming there's a button for logging in with the ID R.id.idBtnTrails
+
+		ActivityScenario.launch(MainActivity.class);
 		Espresso.onView(ViewMatchers.withId(R.id.idBtnTrails)).perform(ViewActions.click());
 
 
 	}
 
 	@Test
-	public void testCreateAccountButton() {
+	public void testLogoutButton() {
 		// Assuming there's a button for creating an account with the ID R.id.idBtnSendData
 		Espresso.onView(ViewMatchers.withId(R.id.idBtnSendData)).perform(ViewActions.click());
 
 
-	}
-
-	private void signOut() {
-		// Sign out the current user
-		firebaseAuth.signOut();
 	}
 
 	@Test
@@ -148,14 +121,4 @@ public class ApplicationTest {
 			}
 		});
 	}
-
-	@Test
-	public void testDisplayTrailSuccessful() {
-
-		Espresso.onView(ViewMatchers.withId(R.id.draw_path)).perform(ViewActions.click());
-
-		Espresso.onView(ViewMatchers.withId(R.id.list_view)).check(matches(isDisplayed()));
-	}
-
-
 }
